e03bdc3c83ff104f9b21c2f2fead5719b8d51d0e
Anatoliy Dmitriev <anatoliy.dmitriev06@mail.ru>
2022-11-27 19:31:18 +0700

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('discordButton');
+
+button.addEventListener("mouseover", function(){
+    button.textContent = "Ted391#6152";
+});
+
+button.addEventListener("mouseout", function(){
+    button.textContent = "Discord";
+});
